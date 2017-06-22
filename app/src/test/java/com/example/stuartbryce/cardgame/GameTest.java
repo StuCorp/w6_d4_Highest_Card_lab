cacb36071d8aecc735c80b3349d7f2b8ee17f4fe
stuartbryce <stuart.l.bryce@gmail.com>
2017-06-22 16:18:05 +0100

    tests starting to use mockito

--- app/src/test/java/com/example/stuartbryce/cardgame/GameTest.java
+++ app/src/test/java/com/example/stuartbryce/cardgame/GameTest.java
@@ -2,10 +2,15 @@ package com.example.stuartbryce.cardgame;
 import org.mockito.*;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import cards.CardSuit;
+import cards.CardValue;
+import dalvik.annotation.TestTargetClass;
 
 import static org.junit.Assert.assertEquals;
 /**
@@ -18,13 +23,19 @@ public class GameTest {
     Player player2;
     Dealer dealer;
     Game game;
+    Card card1;
+    Card card2;
+    Dealer spyDealer;
 
     @Before
     public void before(){
         player1 = new Player("Stuart");
         player2 = new Player("Absent Sam");
         dealer = new Dealer();
-        game = new Game(player1, player2, dealer);
+        spyDealer = Mockito.spy(dealer);
+        game = new Game(player1, player2, spyDealer);
+        card1 = new Card(CardSuit.DIAMONDS, CardValue.ACE);
+        card2 = new Card(CardSuit.HEARTS, CardValue.EIGHT);
     }
 
     @Test
@@ -32,6 +43,34 @@ public class GameTest {
         assertEquals("Stuart", player1.getName());
     }
 
+    @Test
+    public void testForceCardIn(){
+
+        Mockito.when(spyDealer.deal()).thenReturn(card1);
+        game.run();
+        assertEquals(card1, player1.getCurrentCard());
+    }
+
+
+
+
+//    Game game;
+//    Rollable spyDice;
+//
+//
+//    @Before
+//    public void before(){
+//        Rollable dice = new Dice(6);
+//        spyDice = Mockito.spy(dice);
+//        game = new Game(spyDice);
+//    }
+//
+//    @Test
+//    public void testTakeTurnPass(){
+//        Mockito.when(spyDice.roll()).thenReturn(4);
+//        boolean result = game.nextTurn();
+//        assertEquals(true, result);
+//    }
 
 
 }
